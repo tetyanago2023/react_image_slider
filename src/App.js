3ecdc22ccb9eab29a91544a138d72bb96a7b3a86
go2023 <tetyanago2018@gmail.com>
2024-02-07 19:47:56 -0800

    Next step of imageSlider stuff implementation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import ImageSlider from "./components/image-slider";
 
 function App() {
   return (
     <div className="App">
-
+      {/* Image slider component */}
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
     </div>
   );
 }
--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,27 +1,35 @@
 import {useEffect, useState} from "react";
 
 
-export default function ImageSlider ({ url, limit }) {
+export default function ImageSlider ({ url, limit = 5, page = 1 }) {
 
     const [images, setImages] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
-    const [errorMsg, setErrorMsg] = useState(null)
+    const [errorMsg, setErrorMsg] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     async function fetchImages(getUrl) {
         try {
-            const response = await fetch(getUrl);
+            setLoading(true);
+            const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
             const data = await response.json();
             if (data) {
                 setImages(data);
+                setLoading(false);
             }
         } catch (e) {
             setErrorMsg(e.message)
+            setLoading(false);
         }
     }
 
     useEffect(() => {
         if (url !== "") fetchImages(url)
     }, []);
+
+    if (loading) return <p>Loading...</p>
+
+    if (errorMsg) return <p>Error occurred: {errorMsg}</p>
 
     return (
         <div className="container">

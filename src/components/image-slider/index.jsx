f7fb29fedbbe21a890a2906ac9e6fe4543aca885
go2023 <tetyanago2018@gmail.com>
2024-02-08 17:41:41 -0800

    code was refactored

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -1,19 +1,16 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 import "./styles.css";
 
-
-export default function ImageSlider ({ url, limit = 5, page = 1 }) {
-
+export default function ImageSlider({ url, limit = 5, page = 1 }) {
     const [images, setImages] = useState([]);
     const [currentSlide, setCurrentSlide] = useState(0);
     const [errorMsg, setErrorMsg] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    async function fetchImages(getUrl) {
+    const fetchImages = async (getUrl) => {
         setLoading(true);
         try {
-            setLoading(true);
             const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
             const data = await response.json();
             if (data) {
@@ -21,62 +18,56 @@ export default function ImageSlider ({ url, limit = 5, page = 1 }) {
                 setLoading(false);
             }
         } catch (e) {
-            setErrorMsg(e.message)
+            setErrorMsg(e.message);
             setLoading(false);
         }
-    }
+    };
 
     const handlePrevious = () => {
-        setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
-    }
+        setCurrentSlide((prevSlide) => (prevSlide === 0 ? images.length - 1 : prevSlide - 1));
+    };
 
     const handleNext = () => {
-        setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
-    }
+        setCurrentSlide((prevSlide) => (prevSlide === images.length - 1 ? 0 : prevSlide + 1));
+    };
 
     useEffect(() => {
-        if (url !== "") fetchImages(url)
-    }, []);
+        if (url !== "") fetchImages(url);
+    }, [url, page, limit]);
 
-    if (loading) return <p>Loading...</p>
+    if (loading) return <p>Loading...</p>;
 
-    if (errorMsg) return <p>Error occurred: {errorMsg}</p>
+    if (errorMsg) return <p>Error occurred: {errorMsg}</p>;
 
     return (
         <div className="container">
-            <BsArrowLeftCircleFill onClick={handlePrevious} className={"arrow arrow-left"}/>
-            {
-                images && images.length
-                    ? images.map((imageItem, index) =>
-                        <img
-                            key={imageItem.id}
-                            alt={imageItem.download_url}
-                            src={imageItem.download_url}
-                            className={
-                                currentSlide === index
-                                    ? "current-image"
-                                    : "current-image hide-current-image"
-                            }
-                        />
-                    )
-                    : null
-            }
-            <BsArrowRightCircleFill onClick={handleNext} className={"arrow arrow-right"}/>
+            <BsArrowLeftCircleFill onClick={handlePrevious} className="arrow arrow-left" />
+            {images && images.length ? (
+                images.map((imageItem, index) => (
+                    <img
+                        key={imageItem.id}
+                        alt={imageItem.download_url}
+                        src={imageItem.download_url}
+                        className={`current-image ${
+                            currentSlide === index ? "" : "hide-current-image"
+                        }`}
+                    />
+                ))
+            ) : null}
+            <BsArrowRightCircleFill onClick={handleNext} className="arrow arrow-right" />
             <span className="circle-indicators">
-                {images && images.length
-                    ? images.map((_, index) => (
+                {images && images.length ? (
+                    images.map((_, index) => (
                         <button
                             key={index}
-                            className={
-                                currentSlide === index
-                                    ? "current-indicator"
-                                    : "current-indicator inactive-indicator"
-                            }
+                            className={`current-indicator ${
+                                currentSlide === index ? "" : "inactive-indicator"
+                            }`}
                             onClick={() => setCurrentSlide(index)}
                         ></button>
                     ))
-                    : null}
+                ) : null}
             </span>
         </div>
-    )
+    );
 }
